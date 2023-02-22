07f4745b03d5d0e7a4b9677123dcb9cfc49a6a30
reid501 <alexatrs@hotmail.co.uk>
2023-02-22 17:05:47 +0000

    project and tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        vuejs: {
+          100: "#49e659",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
