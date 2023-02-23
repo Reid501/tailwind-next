004a980b0491e64b9d7d080f7b9df2cb162796ad
reid501 <alexatrs@hotmail.co.uk>
2023-02-23 14:08:20 +0000

    base style and media queries added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,12 @@ export default function Home() {
     <>
       <h1 className="border-4 border-green-600">Hello world!</h1>
       <h1 className="text-vuejs-100">Hello world!</h1>
+
+      <div>
+        <p className="text-xs md:text-lg text-vuejs-100 lg:text-2xl lg:text-[#949494]">
+          Hello long sentence
+        </p>
+      </div>
     </>
   );
 }
